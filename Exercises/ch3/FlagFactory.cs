db0fc84cf87db50cdc13a1f7045068e8350cea98
Ans Uddin <ansuddin7@gmail.com>
2020-01-29 13:22:32 +0100

    flag solution

--- Exercises/ch3/FlagFactory.cs
+++ Exercises/ch3/FlagFactory.cs
@@ -1,40 +1,35 @@
-using System.Collections.Generic;
 using System.Drawing;
+using System.Collections.Generic;
 
 namespace eu.sig.training.ch03
 {
 
     public class FlagFactory
     {
+
         // tag::getFlag[]
+        private static Dictionary<Nationality, IList<Color>> FLAGS =
+            new Dictionary<Nationality, IList<Color>>();
+
+        static FlagFactory()
+        {
+            FLAGS[Nationality.DUTCH] = new List<Color>{ Color.Red, Color.White,
+                Color.Blue };
+            FLAGS[Nationality.GERMAN] = new List<Color>{ Color.Black, Color.Red,
+                Color.Yellow };
+            FLAGS[Nationality.BELGIAN] = new List<Color>{ Color.Black, Color.Yellow,
+                Color.Red };
+            FLAGS[Nationality.FRENCH] = new List<Color>{ Color.Blue, Color.White,
+                Color.Red };
+            FLAGS[Nationality.ITALIAN] = new List<Color>{ Color.Green, Color.White,
+                Color.Red };
+        }
+
         public IList<Color> GetFlagColors(Nationality nationality)
         {
-            List<Color> result;
-            switch (nationality)
-            {
-                case Nationality.DUTCH:
-                    result = new List<Color> { Color.Red, Color.White, Color.Blue };
-                    break;
-                case Nationality.GERMAN:
-                    result = new List<Color> { Color.Black, Color.Red, Color.Yellow };
-                    break;
-                case Nationality.BELGIAN:
-                    result = new List<Color> { Color.Black, Color.Yellow, Color.Red };
-                    break;
-                case Nationality.FRENCH:
-                    result = new List<Color> { Color.Blue, Color.White, Color.Red };
-                    break;
-                case Nationality.ITALIAN:
-                    result = new List<Color> { Color.Green, Color.White, Color.Red };
-                    break;
-                case Nationality.UNCLASSIFIED:
-                default:
-                    result = new List<Color> { Color.Gray };
-                    break;
-            }
-            return result;
+            IList<Color> colors = FLAGS[nationality];
+            return colors ?? new List<Color> { Color.Gray };
         }
         // end::getFlag[]
     }
-
 }
